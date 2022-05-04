c05c079767f02d0dcc033e81fd0dbe8fb23ffe97
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-05-04 22:24:46 +0530

    n=int(input()) a=[n] a=list(map(int,input().split())) for i in range(0,n):     if a[i]!=0:         print(a[i],end=' ') for i in range(0,n):     if a[i]==0:         print(a[i],end=' ')

---
+++ Move_Zeroes.py
@@ -0,0 +1,9 @@
+n=int(input())
+a=[n]
+a=list(map(int,input().split()))
+for i in range(0,n):
+    if a[i]!=0:
+        print(a[i],end=' ')
+for i in range(0,n):
+    if a[i]==0:
+        print(a[i],end=' ')
