097bb8fb04d9478473ef971ef9ba41ec4affc8d3
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-05-18 11:27:34 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 d=0 for i in range(0,n):     if i%2==0:         s+=a[i]     else:         d+=a[i] if abs(s-d)%4==0:     print('X') else:     print('Y')

---
+++ Predict_the_winner.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+d=0
+for i in range(0,n):
+    if i%2==0:
+        s+=a[i]
+    else:
+        d+=a[i]
+if abs(s-d)%4==0:
+    print('X')
+else:
+    print('Y')
+        
