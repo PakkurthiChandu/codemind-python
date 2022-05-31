f71c18f69e5ed1e65913ea249d94227120ebdd42
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-05-31 20:02:45 +0530

    a=input().lower() b=input().lower() f='' for i in range(0,len(a)):     if a[i] not in b and a[i] not in f and a[i]!=' ':         f+=a[i] for i in range(0,len(b)):     if b[i] not in a and b[i] not in f and b[i]!=' ':         f+=b[i] print(len(f))

---
+++ Non_common_characters.py
@@ -0,0 +1,10 @@
+a=input().lower()
+b=input().lower()
+f=''
+for i in range(0,len(a)):
+    if a[i] not in b and a[i] not in f and a[i]!=' ':
+        f+=a[i]
+for i in range(0,len(b)):
+    if b[i] not in a and b[i] not in f and b[i]!=' ':
+        f+=b[i]
+print(len(f))
