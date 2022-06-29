daa68d4e84c7610564613cdb282a7d42d63b090f
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-06-29 15:20:01 +0530

    def prime(a):     if a==1:         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) b=int(input()) c=a+b while 1:     c+=1     if prime(c):         print(c-(a+b))         break

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,16 +1,15 @@
+def prime(a):
+    if a==1:
+        return 0
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
 a=int(input())
 b=int(input())
-c=a+b+1
-te=c
+c=a+b
 while 1:
-    t=c
-    k=0
-    for i in range(2,t):
-        if t%i==0:
-            k+=1
-            break
-    if k==0:
-        #print(c)
-        break
     c+=1
-print("%d"%(c-te+1))
+    if prime(c):
+        print(c-(a+b))
+        break
