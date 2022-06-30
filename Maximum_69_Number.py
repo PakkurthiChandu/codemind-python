78d02935eedfa0b403f2bc0829f81b70156edc5a
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-06-30 15:59:33 +0530

    a=int(input()) t=a r=0 while t!=0:     b=t%10     t=t//10     r=r*10+b rev=0 c=0 while r!=0:     b=r%10     r=r//10     if b==6 and c==0:         b=9         c=1     rev=rev*10+b print(rev)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,17 +1,17 @@
 a=int(input())
+t=a
 r=0
+while t!=0:
+    b=t%10
+    t=t//10
+    r=r*10+b
 rev=0
 c=0
-while a!=0:
-    b=a%10
-    a=a//10
-    r=r*10+b
 while r!=0:
     b=r%10
     r=r//10
-    if c==0:
-        if b==6:
-            b=9
-            c=1
+    if b==6 and c==0:
+        b=9
+        c=1
     rev=rev*10+b
 print(rev)
