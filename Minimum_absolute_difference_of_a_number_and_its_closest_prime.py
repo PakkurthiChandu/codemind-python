b268dccc6b43a94e1c8b2f16d031bcfb26adbf87
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-05 14:47:32 +0530

    def prime(a):     if(a==1):         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) for j in range(a,a+100):     if prime(j):         break for k in range(a,a-100,-1):     if prime(k):         break; if (a-k)<=(j-a):     print(a-k) else:     print(j-a)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,18 +1,18 @@
 def prime(a):
-    if a==1:
+    if(a==1):
         return 0
     for i in range(2,int(a**0.5)+1):
         if a%i==0:
             return 0
     return 1
 a=int(input())
-for i in range(a,a+100):
-    if prime(i):
-        break
-for j in range(a,a-100,-1):
+for j in range(a,a+100):
     if prime(j):
         break
-if (a-j)<=(i-a):
-    print(a-j)
+for k in range(a,a-100,-1):
+    if prime(k):
+        break;
+if (a-k)<=(j-a):
+    print(a-k)
 else:
-    print(i-a)
+    print(j-a)
