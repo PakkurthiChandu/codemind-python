9cf876c765f8ee5e094509d34b7da596e3d1041c
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-05 14:55:51 +0530

    a=int(input()) f=0 s=1 while f<a:     t=f     f=s     s=t+s if(a-t)==(f-a):     print(t,f) elif (a-t)>(f-a):     print(f) else:     print(t)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,16 +1,13 @@
 a=int(input())
 f=0
 s=1
-for i in range(3,a):
-    b=f+s
+while f<a:
+    t=f
     f=s
-    s=b
-    if b>a:
-        break
-if (a-f)<(s-a):
+    s=t+s
+if(a-t)==(f-a):
+    print(t,f)
+elif (a-t)>(f-a):
     print(f)
-elif((a-f)==(s-a)):
-    print(f,end=' ')
-    print(s)
 else:
-    print(s)
+    print(t)
