2ebc1c6225ef21c2a8a3058489b8ba316bbb59cb
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-05 14:27:26 +0530

    def prime(a):     if(a==1):         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 n=int(input()) for i in range(0,n):     a=int(input())     for j in range(a,a+100):         if prime(j):             break     for k in range(a,a-100,-1):         if prime(k):             break;     if (a-k)<=(j-a):         print(k)     else:         print(j)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,5 +1,5 @@
 def prime(a):
-    if a==1:
+    if(a==1):
         return 0
     for i in range(2,int(a**0.5)+1):
         if a%i==0:
@@ -8,13 +8,13 @@ def prime(a):
 n=int(input())
 for i in range(0,n):
     a=int(input())
-    for j in range(a,a-20,-1):
+    for j in range(a,a+100):
         if prime(j):
             break
-    for k in range(a,a+20):
+    for k in range(a,a-100,-1):
         if prime(k):
-            break
-    if (a-j)<=(k-a):
-        print(j)
-    else:
+            break;
+    if (a-k)<=(j-a):
         print(k)
+    else:
+        print(j)
