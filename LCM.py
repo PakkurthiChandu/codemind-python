643872431c75aa8f7478d6b49c13cf1890e3cbb6
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-05 15:06:28 +0530

    a,b=map(int,input().split()) c=max(a,b) i=1 while 1:     if (c*i)%a==0 and (c*i)%b==0:         print(c*i)         break     i+=1

--- LCM.py
+++ LCM.py
@@ -1,9 +1,8 @@
 a,b=map(int,input().split())
 c=max(a,b)
-d=min(a,b)
-for i in range(1,d+1):
-    e=c*i
-    if e%a==0 and e%b==0:
-        print(e)
-        break;
-    
+i=1
+while 1:
+    if (c*i)%a==0 and (c*i)%b==0:
+        print(c*i)
+        break
+    i+=1
