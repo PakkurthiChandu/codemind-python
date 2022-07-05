f669cc9d3992872eed3b1b0f1bf1d34e60782e65
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-05 15:10:04 +0530

    a=int(input()) c=0 if(a<0):     c=1     a=-a b=str(a) b=b[::-1] a=int(b) if(c==0):     print(a) else:     print(-a)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,15 +1,12 @@
 a=int(input())
-rev=0
 c=0
-if a<0:
-    a=-1*a
-    c+=1
-while a!=0:
-    b=a%10
-    a=a//10
-    rev=rev*10+b
+if(a<0):
+    c=1
+    a=-a
+b=str(a)
+b=b[::-1]
+a=int(b)
 if(c==0):
-    print(rev)
+    print(a)
 else:
-    print(-1*rev)
-
+    print(-a)
