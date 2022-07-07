3bb4916495ece34d82de59eab95a5159b2f3bd1f
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-07 14:48:12 +0530

    a=int(input()) f=0 s=1 c=1 print(f,s,end=' ') while c<=a-2:     t=f+s     print(t,end=' ')     f=s     s=t     c+=1

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,11 @@
 a=int(input())
 f=0
 s=1
-print(f,end=' ')
-print(s,end=' ')
-for i in range(3,a+1):
-    b=f+s
-    print(b,end=' ')
+c=1
+print(f,s,end=' ')
+while c<=a-2:
+    t=f+s
+    print(t,end=' ')
     f=s
-    s=b
+    s=t
+    c+=1
