0e860e20e00325e654f91f06c4dcaeb430ad191b
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-07 14:45:10 +0530

    a=int(input()) f=0 s=1 t=0 while t<a:     t=f+s     f=s     s=t if t==a:     print('True') else:     print('False')

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,15 +1,12 @@
 a=int(input())
 f=0
 s=1
-b=0
-c=0
-while b<=a:
-    b=f+s
+t=0
+while t<a:
+    t=f+s
     f=s
-    s=b
-    if f==a:
-        c=1
-if c==1:
+    s=t
+if t==a:
     print('True')
 else:
     print('False')
