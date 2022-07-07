867bdb5184ce3783ef87c22d0fbdfdd87be1e6f8
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-07 14:35:02 +0530

    a,b=map(int,input().split()) c=min(a,b) while c>0:     if a%c==0 and b%c==0:         print(c)         break     c-=1

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,6 +1,7 @@
 a,b=map(int,input().split())
 c=min(a,b)
-for i in range(c,0,-1):
-    if a%i==0 and b%i==0:
-        print(i)
+while c>0:
+    if a%c==0 and b%c==0:
+        print(c)
         break
+    c-=1
