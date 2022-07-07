e38c81bbbdacf75dbd7d3bd56a2683a74d27afe1
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-07 14:51:20 +0530

    a=int(input()) n=len(str(a)) s=0 t=a while a!=0:     b=a%10     a=a//10     s+=b**n     n-=1 if s==t:     print('True') else:     print('False')

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,21 +1,13 @@
 a=int(input())
-r=0
-t=a
-te=a
+n=len(str(a))
 s=0
-i=0
+t=a
 while a!=0:
     b=a%10
     a=a//10
-    r=r*10+b
-    i+=1
-while t!=0:
-    c=t%10
-    t=t//10
-    s+=pow(c,i)
-    i-=1
-if te==s:
+    s+=b**n
+    n-=1
+if s==t:
     print('True')
 else:
     print('False')
-
