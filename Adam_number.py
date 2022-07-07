ad2671e593fc3c2b7e000ac41aba1a53f34d8acf
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-07 14:30:59 +0530

    a=int(input()) b=a*a a=str(a) a=a[::-1] a=int(a) c=a*a c=str(c) c=c[::-1] c=int(c) if b==c:     print('True') else:     print('False')

--- Adam_number.py
+++ Adam_number.py
@@ -1,17 +1,13 @@
 a=int(input())
 b=a*a
-rev=0
-r=0
-while a!=0:
-    c=a%10
-    a=a//10
-    rev=rev*10+c
-d=rev*rev
-while d!=0:
-    e=d%10
-    d=d//10
-    r=r*10+e
-if r==b:
+a=str(a)
+a=a[::-1]
+a=int(a)
+c=a*a
+c=str(c)
+c=c[::-1]
+c=int(c)
+if b==c:
     print('True')
 else:
     print('False')
