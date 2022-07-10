02a3952f5ed603fa5e9fe74b9cac6d61a1ae4145
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 14:20:09 +0530

    def pal(a):     b=str(a)     b=b[::-1]     b=int(b)     return b n=int(input()) a=list(map(int,input().split())) d=0 for i in a:     print(pal(i),end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,17 +1,10 @@
-def rev(a):
-    r=0
-    while a!=0:
-        b=a%10
-        a=a//10
-        r=r*10+b
-    return r
+def pal(a):
+    b=str(a)
+    b=b[::-1]
+    b=int(b)
+    return b
 n=int(input())
-a=[n]
-b=[n]
 a=list(map(int,input().split()))
-from array import*
-for i in range(0,n):
-    x=rev(a[i])
-    b.append(x)
-for i in range(1,n+1):
-    print(b[i],end=' ')
+d=0
+for i in a:
+    print(pal(i),end=' ')
