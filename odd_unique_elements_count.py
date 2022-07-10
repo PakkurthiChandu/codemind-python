4adc8876217fe13e9c6708c0112afbf7baa63e04
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 20:58:07 +0530

    n=int(input()) a=list(map(int,input().split())) a=list(set(a)) c=0 for i in a:     if i%2!=0:         c+=1 print(c)

--- odd_unique_elements_count.py
+++ odd_unique_elements_count.py
@@ -1,14 +1,8 @@
 n=int(input())
-a=[n]
 a=list(map(int,input().split()))
+a=list(set(a))
 c=0
-k=0
-for i in range(0,n):
-    for j in range(0,n):
-        if a[i]==a[j]:
-            c+=1
-    if c==1:
-        if a[i]%2!=0:
-            k+=1
-    c=0
-print(k)
+for i in a:
+    if i%2!=0:
+        c+=1
+print(c)
