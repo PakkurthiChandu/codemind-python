c21bdfafd06b80ff4bd5da3edc65faca8a7a3506
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 20:32:50 +0530

    n=int(input()) a=list(map(int,input().split())) b,c=map(int,input().split()) d=0 for i in a:     if i>=b and i<=c:         d=1         print(i,end=' ') if d==0:     print('-1')

--- elements_in_between_A_and_B.py
+++ elements_in_between_A_and_B.py
@@ -1,11 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
 b,c=map(int,input().split())
-s=0
 d=0
-for i in range(0,n):
-    if a[i]>=b and a[i]<=c:
-        print(a[i],end=' ')
+for i in a:
+    if i>=b and i<=c:
         d=1
+        print(i,end=' ')
 if d==0:
     print('-1')
