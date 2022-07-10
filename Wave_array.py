a18e3791370a8d23de3007e636f739840c4d76a5
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 14:30:27 +0530

    n=int(input()) a=list(map(int,input().split())) if a[0]<a[1]:     d=0     for i in range(1,n-1,2):         if a[i-1]>a[i] or a[i]<a[i+1]:             d=1             break     if d==1:         print('no')     else:         print('yes') else:     d=0     for i in range(1,n-1,2):         if a[i-1]<a[i] or a[i]>a[i+1]:             d=1             break     if d==1:         print('no')     else:         print('yes')

--- Wave_array.py
+++ Wave_array.py
@@ -1,16 +1,22 @@
 n=int(input())
 a=list(map(int,input().split()))
 if a[0]<a[1]:
+    d=0
     for i in range(1,n-1,2):
-        if a[i]<a[i-1] or a[i]<a[i+1]:
-            print('no')
+        if a[i-1]>a[i] or a[i]<a[i+1]:
+            d=1
             break
+    if d==1:
+        print('no')
     else:
         print('yes')
 else:
+    d=0
     for i in range(1,n-1,2):
-        if a[i]>a[i-1] or a[i]>a[i+1]:
-            print('no')
+        if a[i-1]<a[i] or a[i]>a[i+1]:
+            d=1
             break
+    if d==1:
+        print('no')
     else:
         print('yes')
