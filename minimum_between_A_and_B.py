f8c1cb7701c449af33737f6eff908ccf99a42300
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 20:25:32 +0530

    n=int(input()) a=list(map(int,input().split())) b,c=map(int,input().split()) m=99999999999999999 for i in a:     if i>=b and i<=c:         if i<m:             m=i if m==99999999999999999:     print('-1') else:     print(m)

--- minimum_between_A_and_B.py
+++ minimum_between_A_and_B.py
@@ -1,14 +1,12 @@
 n=int(input())
 a=list(map(int,input().split()))
 b,c=map(int,input().split())
-m=9999999
-d=0
-for i in range(0,n):
-    if a[i]>=b and a[i]<=c:
-        if a[i]<m:
-            m=a[i]
-            d=1
-if d==0:
-    print("-1")
+m=99999999999999999
+for i in a:
+    if i>=b and i<=c:
+        if i<m:
+            m=i
+if m==99999999999999999:
+    print('-1')
 else:
     print(m)
