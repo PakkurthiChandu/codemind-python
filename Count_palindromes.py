be6d21e63beb2cecf9b70ea02d262ca957436172
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 14:19:00 +0530

    def pal(a):     b=str(a)     b=b[::-1]     b=int(b)     if a==b:         return 1     return 0 n=int(input()) a=list(map(int,input().split())) d=0 for i in a:     d+=pal(i) print(d)

--- Count_palindromes.py
+++ Count_palindromes.py
@@ -1,20 +1,13 @@
 def pal(a):
-    t=a
-    r=0
-    while a!=0:
-        b=a%10
-        a=a//10
-        r=r*10+b
-    if t==r:
+    b=str(a)
+    b=b[::-1]
+    b=int(b)
+    if a==b:
         return 1
-    else:
-        return 0
+    return 0
 n=int(input())
-c=0
-a=[n]
 a=list(map(int,input().split()))
-for i in range(0,n):
-    if pal(a[i]):
-        c+=1
-print(c)
-        
+d=0
+for i in a:
+    d+=pal(i)
+print(d)
