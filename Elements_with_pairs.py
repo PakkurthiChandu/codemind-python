1038d08e610d88e1571701bf68b6f9c08496eeb8
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-10 21:04:07 +0530

    n=int(input()) a=list(map(int,input().split())) if n%2==0:     print(*a) else:     print(*a,end=' ')     print('0')

--- Elements_with_pairs.py
+++ Elements_with_pairs.py
@@ -1,7 +1,7 @@
 n=int(input())
-a=[n]
 a=list(map(int,input().split()))
-for i in range(0,n):
-    print(a[i],end=' ')
-if n%2!=0:
+if n%2==0:
+    print(*a)
+else:
+    print(*a,end=' ')
     print('0')
