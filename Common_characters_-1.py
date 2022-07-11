9f3f0417b13a399dfc87cd5204a072b52dc3f601
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 14:41:02 +0530

    a=input().lower() b='' a=a.split(' ') for i in a[0]:     c=0     for j in a:         if i in j:             c+=1     if c==len(a):         b+=i if len(b)==0:     print('-1') else:     print(b)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,13 +1,14 @@
 a=input().lower()
-b=a.split(' ')
-d=0
-for i in range(0,len(b[0])):
+b=''
+a=a.split(' ')
+for i in a[0]:
     c=0
-    for j in range(0,len(b)):
-        if b[0][i] in b[j]:
+    for j in a:
+        if i in j:
             c+=1
-    if c==len(b):
-        print(b[0][i],end='')
-        d=1
-if d==0:
+    if c==len(a):
+        b+=i
+if len(b)==0:
     print('-1')
+else:
+    print(b)
