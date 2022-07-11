90129e7433ac105a877e9c6c1f1df4dcac822ec5
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 15:10:04 +0530

    a=input() a=a.split(' ') b='aeiouAEIOU' c=0 for i in a:     if i[0] in b and i[len(i)-1] not in b:         c+=1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,9 +1,8 @@
 a=input()
-c=a.lower()
-b=c.split()
-d=0
-for i in range(0,len(b)):
-    if(b[i][0]=='a' or b[i][0]=='e' or b[i][0]=='i' or b[i][0]=='o' or b[i][0]=='u') and (b[i][len(b[i])-1]!='a' or b[i][len(b[i])-1]!='e' or b[i][len(b[i])-1]!='i' or b[i][len(b[i])-1]!='o' or b[i][len(b[i]-1)]!='u'):
-        d+=1
-print(d)
-        
+a=a.split(' ')
+b='aeiouAEIOU'
+c=0
+for i in a:
+    if i[0] in b and i[len(i)-1] not in b:
+        c+=1
+print(c)
