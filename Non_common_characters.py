60a27133e0e8c30918a4d08039fdc79d104e1d79
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 14:16:17 +0530

    a=input().lower() b=input().lower() a=a.replace(" ",'') b=b.replace(' ','') c='' for i in a:     if i not in b and i not in c:         c+=i for i in b:     if i not in a and i not in c:         c+=i c=sorted(c) if len(c)==0:     print('-1') else:     print(len(c))

--- Non_common_characters.py
+++ Non_common_characters.py
@@ -1,10 +1,16 @@
 a=input().lower()
 b=input().lower()
-f=''
-for i in range(0,len(a)):
-    if a[i] not in b and a[i] not in f and a[i]!=' ':
-        f+=a[i]
-for i in range(0,len(b)):
-    if b[i] not in a and b[i] not in f and b[i]!=' ':
-        f+=b[i]
-print(len(f))
+a=a.replace(" ",'')
+b=b.replace(' ','')
+c=''
+for i in a:
+    if i not in b and i not in c:
+        c+=i
+for i in b:
+    if i not in a and i not in c:
+        c+=i
+c=sorted(c)
+if len(c)==0:
+    print('-1')
+else:
+    print(len(c))
