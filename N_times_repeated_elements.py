7979937c20fba773c25f7b60299fd627e622100e
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 09:26:27 +0530

    n=int(input()) a=list(map(int,input().split())) b=int(input()) c=[] for i in a:     if i not in c and a.count(i)==b:         c.append(i) if len(c)==0:     print('-1') else:     print(*c)

--- N_times_repeated_elements.py
+++ N_times_repeated_elements.py
@@ -1,11 +1,11 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=int(input())
-c=list(set(a))
-d=0
-for i in range(0,len(c)):
-    if a.count(c[i])==b:
-        print(c[i],end=' ')
-        d=1
-if d==0:
+c=[]
+for i in a:
+    if i not in c and a.count(i)==b:
+        c.append(i)
+if len(c)==0:
     print('-1')
+else:
+    print(*c)
