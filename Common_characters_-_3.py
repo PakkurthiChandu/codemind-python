d73e36b39733355659df7588edae81eec3a06ab5
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 14:24:42 +0530

    a=input().lower() a=a.split(' ') b='' for i in a[0]:     c=0     for j in a:         if i in j:             c+=1     if c==len(a):         b+=i if len(b)==0:     print('-1') else:     b=sorted(b)     print(b[0])

--- Common_characters_-_3.py
+++ Common_characters_-_3.py
@@ -1,16 +1,15 @@
 a=input().lower()
-b=a.split(' ')
-e=0
-d=''
-for i in range(0,len(b[0])):
+a=a.split(' ')
+b=''
+for i in a[0]:
     c=0
-    for j in range(0,len(b)):
-        if b[0][i] in b[j]:
+    for j in a:
+        if i in j:
             c+=1
-    if c==len(b):
-        d+=b[0][i]
-        e=1
-if e==0:
+    if c==len(a):
+        b+=i
+if len(b)==0:
     print('-1')
 else:
-    print(min(d))
+    b=sorted(b)
+    print(b[0])
