e43fca4cee152a23daef5c9f3efe9fcfd940065e
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 17:18:32 +0530

    n=int(input()) a=[] b=[] for i in range(n):     d=list(map(int,input().split()))     a.append(d) for i in range(n):     d=list(map(int,input().split()))     b.append(d) c=[] for i in range(n):     d=[]     for j in range(n):        d.append(abs(a[i][j]-b[i][j]))     c.append(d) for i in range(n):     for j in range(n):         if j==n-1:             print(c[i][j])         else:             print(c[i][j],end=' ')

---
+++ Absolute_difference_of_two_matrices.py
@@ -0,0 +1,21 @@
+n=int(input())
+a=[]
+b=[]
+for i in range(n):
+    d=list(map(int,input().split()))
+    a.append(d)
+for i in range(n):
+    d=list(map(int,input().split()))
+    b.append(d)
+c=[]
+for i in range(n):
+    d=[]
+    for j in range(n):
+       d.append(abs(a[i][j]-b[i][j])) 
+    c.append(d)
+for i in range(n):
+    for j in range(n):
+        if j==n-1:
+            print(c[i][j])
+        else:
+            print(c[i][j],end=' ')
