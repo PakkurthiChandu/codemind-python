e2c05985632edc1b4868d8ed942a7e58c6592555
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-11 14:19:05 +0530

    a=input().lower() b=input().lower() a=a.replace(" ",'') b=b.replace(' ','') c='' for i in a:     if i in b and i not in c:         c+=i c=sorted(c) print(len(c))

--- common_characters_-II.py
+++ common_characters_-II.py
@@ -1,10 +1,10 @@
 a=input().lower()
 b=input().lower()
-f=''
-for i in range(0,len(a)):
-    if a[i] in b and a[i] not in f and a[i]!=' ':
-        f+=a[i]
-for i in range(0,len(b)):
-    if b[i] in a and b[i] not in f and b[i]!=' ':
-        f+=b[i]
-print(len(f))
+a=a.replace(" ",'')
+b=b.replace(' ','')
+c=''
+for i in a:
+    if i in b and i not in c:
+        c+=i
+c=sorted(c)
+print(len(c))
