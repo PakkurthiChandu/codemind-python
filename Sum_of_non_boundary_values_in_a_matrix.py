e29968a49a8b7a2460edf7e0fe4e343e477de167
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-17 22:30:05 +0530

    a,b=map(int,input().split()) c=[] for i in range(a):     d=list(map(int,input().split()))     c.append(d) s=0 for i in range(a):     for j in range(b):         if i!=0 and i!=a-1 and j!=0 and j!=b-1:             s+=c[i][j] print(s)

--- Sum_of_non_boundary_values_in_a_matrix.py
+++ Sum_of_non_boundary_values_in_a_matrix.py
@@ -1,9 +1,9 @@
 a,b=map(int,input().split())
 c=[]
-s=0
 for i in range(a):
     d=list(map(int,input().split()))
     c.append(d)
+s=0
 for i in range(a):
     for j in range(b):
         if i!=0 and i!=a-1 and j!=0 and j!=b-1:
