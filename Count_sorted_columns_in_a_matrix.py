12fd5329759c65bc25abe25a57741aef655aefe5
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-17 22:42:28 +0530

    a,b=map(int,input().split()) c=[] for i in range(a):    d=list(map(int,input().split()))    c.append(d) g=0 for i in range(b):     e=[]     for j in range(a):         e.append(c[j][i])     f=[]     for j in range(len(e)-1,-1,-1):         f.append(e[j])     if e==sorted(e) or f==sorted(e):         g+=1 print(g)

--- Count_sorted_columns_in_a_matrix.py
+++ Count_sorted_columns_in_a_matrix.py
@@ -1,18 +1,16 @@
-n,m=map(int,input().split())
-a=[]
-for i in range(n):
-    b=list(map(int,input().split()))
-    a.append(b)
+a,b=map(int,input().split())
+c=[]
+for i in range(a):
+   d=list(map(int,input().split())) 
+   c.append(d)
 g=0
-for i in range(m):
-    d=[]
-    for j in range(n):
-       d.append(a[j][i])
-    e=sorted(d)
+for i in range(b):
+    e=[]
+    for j in range(a):
+        e.append(c[j][i])
     f=[]
-    for j in range(len(d)-1,-1,-1):
-       f.append(e[j])
-    if e==d or d==f:
+    for j in range(len(e)-1,-1,-1):
+        f.append(e[j])
+    if e==sorted(e) or f==sorted(e):
         g+=1
 print(g)
-        
