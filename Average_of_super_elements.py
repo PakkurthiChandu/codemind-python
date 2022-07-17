6fa2315f4ba73d9c29f29fc8d3f10f2657706914
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-17 22:00:34 +0530

    n=int(input()) a=list(map(int,input().split())) b=list(set(a)) s=0 c=0 for i in b:     if a.count(i)==i:         s+=i         c+=1 if c==0:     print('-1') else:     print("%.2f"%(s/c))

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -1,13 +1,13 @@
 n=int(input())
 a=list(map(int,input().split()))
-c=list(set(a))
+b=list(set(a))
 s=0
-d=0
-for i in range(0,len(c)):
-    if a.count(c[i])==c[i]:
-        s+=c[i]
-        d+=1
-if d==0:
+c=0
+for i in b:
+    if a.count(i)==i:
+        s+=i
+        c+=1
+if c==0:
     print('-1')
 else:
-    print("%.2f"%(s/d))
+    print("%.2f"%(s/c))
