a291548b5584bc7cc2625740b0371e8fdb500d9a
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-17 22:37:22 +0530

    a,b=map(int,input().split()) c=0 for i in range(a):     d=list(map(int,input().split()))     e=[]     for i in range(len(d)-1,-1,-1):         e.append(d[i])     if d==sorted(d) or e==sorted(d):         c+=1 print(c)

--- Count_sorted_rows_in_a_matrix.py
+++ Count_sorted_rows_in_a_matrix.py
@@ -1,12 +1,10 @@
-n,m=map(int,input().split())
-a=[]
-e=0
-for i in range(n):
-    b=list(map(int,input().split()))
-    c=sorted(b)
-    d=[]
-    for i in range(len(c)-1,-1,-1):
-        d.append(c[i])
-    if b==c or d==b:
-        e+=1
-print(e)
+a,b=map(int,input().split())
+c=0
+for i in range(a):
+    d=list(map(int,input().split()))
+    e=[]
+    for i in range(len(d)-1,-1,-1):
+        e.append(d[i])
+    if d==sorted(d) or e==sorted(d):
+        c+=1
+print(c)
