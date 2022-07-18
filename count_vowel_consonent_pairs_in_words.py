a07d16202af69c6c2bce943378eeaeac521fd6a5
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-07-18 20:08:30 +0530

    a=input() a=a.split() v='aeiouAEIOU' c=0 for i in a:     j=len(i)-1     k=0     while k<=j:         if (i[k] in v and i[j] not in v) or (i[k] not in v and i[j] in v):             c+=1         j-=1         k+=1 print(c)

--- count_vowel_consonent_pairs_in_words.py
+++ count_vowel_consonent_pairs_in_words.py
@@ -1,13 +1,15 @@
 a=input()
-v='AEIOUaeiou'
 a=a.split()
+v='aeiouAEIOU'
 c=0
 for i in a:
-    j=0
-    k=len(i)-1
-    while j<=k:
-        if (i[j] in v and i[k] not in v) or (i[j] not in v and i[k] in v):
+    j=len(i)-1
+    k=0
+    while k<=j:
+        if (i[k] in v and i[j] not in v) or (i[k] not in v and i[j] in v):
             c+=1
-        j+=1
-        k-=1
+        j-=1
+        k+=1
 print(c)
+            
+        
