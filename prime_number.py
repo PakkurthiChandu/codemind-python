8d6e4c8e7e1aa690639dc68dd8e7eb1b43458a14
PakkurthiChandu <96738063+PakkurthiChandu@users.noreply.github.com>
2022-08-30 08:13:40 +0530

    def prime(a):     if a==1:         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     else:         return 1 a=int(input()) if prime(a):     print('prime') else:     print('not a prime')

--- prime_number.py
+++ prime_number.py
@@ -1,12 +1,13 @@
-a=int(input())
-c=0
-if a==1:
-    c==1
-else:
-    for i in range(2,a):
+def prime(a):
+    if a==1:
+        return 0
+    for i in range(2,int(a**0.5)+1):
         if a%i==0:
-            c=1
-if c==0:
+            return 0
+    else:
+        return 1
+a=int(input())
+if prime(a):
     print('prime')
 else:
     print('not a prime')
